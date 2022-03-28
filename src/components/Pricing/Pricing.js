a6d9fbd3f575decb2af71b2e93fc7e451774ef8e
AkashAbrrar <mabrrar9@gmail.com>
2022-03-28 23:47:47 +0600

    this commit for pricing

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import PricingOption from '../PricingOption/PricingOption';
 
 const Pricing = () => {
+    const pricingOption = [
+        { id: 1, name: 'free', price: 0 },
+        { id: 2, name: 'regular', price: 5000 },
+        { id: 3, name: 'premium', price: 4200 }
+    ];
     return (
         <div className='bg-teal-200 p-4 mt-8'>
-            <h1 className='text-5xl'>Best Deals Of The Town</h1>
+            <h1 className='text-5xl font-mono'>Best Deals Of The Town</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ratione distinctio debitis nemo, autem facilis quo fugiat doloribus perferendis enim?</p>
+            <div>
+                {
+                    pricingOption.map(option => <PricingOption key={option.id}
+                        option={option}
+                    ></PricingOption>)
+                }
+            </div>
         </div>
     );
 };
