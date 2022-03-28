b47afe3f32c52b8f008324ed5f7fb7c7192c5663
AkashAbrrar <mabrrar9@gmail.com>
2022-03-28 23:34:34 +0600

    this is the example of responsive nav bar by tailwind

---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div className='bg-teal-200 p-4 mt-8'>
+            <h1 className='text-5xl'>Best Deals Of The Town</h1>
+        </div>
+    );
+};
+
+export default Pricing;
