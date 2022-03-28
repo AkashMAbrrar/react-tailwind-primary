daa9c4ab29acd2a40ac6f23881c839cba4423f85
AkashAbrrar <mabrrar9@gmail.com>
2022-03-28 23:56:16 +0600

    this commit for price styles

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -11,7 +11,7 @@ const Pricing = () => {
         <div className='bg-teal-200 p-4 mt-8'>
             <h1 className='text-5xl font-mono'>Best Deals Of The Town</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ratione distinctio debitis nemo, autem facilis quo fugiat doloribus perferendis enim?</p>
-            <div>
+            <div className='grid md:grid-cols-3 gap-3 mt-8'>
                 {
                     pricingOption.map(option => <PricingOption key={option.id}
                         option={option}
--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -3,8 +3,9 @@ import React from 'react';
 const PricingOption = (props) => {
     const { name, price } = props.option;
     return (
-        <div className='bg-white p-4'>
-            <h1>{name}</h1>
+        <div className='bg-white p-4 rounded-lg'>
+            <h1 className='bg-teal-200 py-2 rounded text-xl font-bold'>{name}</h1>
+            <span className='text-5xl font-bold'>{price}</span>
         </div>
     );
 };
